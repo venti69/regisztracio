b6a959d9f9c8c193f2dcda153047b9c90bf00955
vtutusk <nagyvati.romeo@szbiszeged.hu>
2024-11-21 09:36:06 +0100

    nincskesz

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3500;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(cors());
 // User model létrehozása
 const UserSchema = new mongoose.Schema(
     {
@@ -33,3 +37,13 @@ mongoose
     .catch((error) => {
         console.log(error.message);
     });
+app.post('/register', async (req, res) => {
+    const {email, password} = req.body;
+    console.log(req.body);
+    
+    try {
+        res.status(200).json({msg: 'Sikeres regisztráció!'});
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
+})
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
 const Register = () => {
-    return <div>Register</div>;
+    const feltolt = async (event) =>{
+        event.preventDefault();
+        const email = document.querySelector('#email').value
+        const password = document.querySelector('#password').value
+        console.log("Email:",email, ". Jelszó:",password);
+        const response = await fetch('http://localhost:3500/register', {
+            method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({email, password})
+        })
+        const valasz = await response.json()
+        if(response.ok){
+            window.alert(valasz.msg)
+        }else{
+            window.alert(valasz.msg)
+        }
+    }
+    return(
+        <div>
+            <form>
+                <label htmlFor="email">Email: </label>
+                <input type="text" id='email'/>
+                <br />
+                <label htmlFor="password">Jelszó: </label>
+                <input type="password" id='password'/>
+                <br />
+                <button onClick={feltolt}>Feltölt</button>
+            </form>
+        </div>)
 };
 
 export default Register;
